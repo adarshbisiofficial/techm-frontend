56ea69c47a427913a2f3ef75306e7c292b9d11f3
Adarsh Bisi <imadarshbisi@gmail.com>
2019-06-28 00:03:28 +0530

    added background color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import { Container } from "reactstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {Helmet} from 'react-helmet';
+
 
 //pages
 import Home from "./pages/home";
@@ -11,6 +13,9 @@ import Signup from "./pages/signup";
 function App() {
   return (
     <div className="App">
+    <Helmet>
+      <style>{'body { background-color: #DAE0E2; }'}</style>
+    </Helmet>
       <Container />
       <Router>
         <Route path="/" exact component={Home} />
