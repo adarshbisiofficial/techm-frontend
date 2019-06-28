240d05f4fb93f240beccf4765ba2f730b5e8d1f6
Adarsh Bisi <imadarshbisi@gmail.com>
2019-06-28 14:08:00 +0530

    final ui

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ import Axios from "axios";
 import Admin from "../components/admin";
 import Staff from "../components/staff";
 import "./home.css";
-import {Jumbotron,Navbar} from 'react-bootstrap';
+import {Jumbotron,Navbar,Nav,FormControl,Form} from 'react-bootstrap';
 //import {Image,Col,Row} from 'react-bootstrap';
 //import {logo} from "../logo-resize.png";
 
@@ -46,10 +46,19 @@ class Home extends Component {
     const { user, loggedin } = this.state;
     return (
       <div className="home">
-        <Container>
-          <br />
-          <h2>ScotiaBank</h2>
-          <br />
+      <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">ScotiaBank.</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Pricing</Nav.Link>
+      <Nav.Link href="#pricing">Help</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+        <Container><br/><br/>
           <Jumbotron>
               <h1>Welcome to Scotiabank.</h1>
               <p>We offer personal and commercial banking, wealth management and private banking, corporate and investment banking,<br/> and capital markets, through our global team of approximately 90,000 Scotiabankers.</p>
@@ -75,7 +84,7 @@ class Home extends Component {
             <div>{user === "admin" ? <Admin /> : <Staff />}</div>
           ) : null}
         </Container>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/>
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="#home">
       {'© Scotiabank. All Rights Reserved'}
